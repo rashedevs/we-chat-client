a397331810cdb7be401a55c9d9d3fe7becd5dc07
rashedevs <rasheduap2015@gmail.com>
2022-06-02 15:32:42 +0600

    working on text input field

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import "./App.css";
+import TextEditor from "./Pages/TextEditor";
+
+const config = {
+  buttons: [
+    "bold",
+    "italic",
+    "strikethrough",
+    "link",
+    "ul",
+    "ol",
+    "blockquote",
+    "source",
+  ],
+};
 
 function App() {
+  const [value, setValue] = useState("");
   return (
-    <div className="App">
-      <h2>This is a chat app</h2>
+    <div className="">
+      <h2 className="text-center my-5">Let's Talk</h2>
+      <div>
+        <p>{value}</p>
+      </div>
+      <div>
+        <TextEditor setValue={setValue} config={config}></TextEditor>
+      </div>
     </div>
   );
 }
